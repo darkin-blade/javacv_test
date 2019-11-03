6d50faeec8f081ddf2055713fe214f49ceb52d56
lynx <2104934038@qq.com>
2019-11-03 17:18:50 +0800

    test:合并图片

--- app/src/main/java/com/example/javacv/LocalRecognize.java
+++ app/src/main/java/com/example/javacv/LocalRecognize.java
@@ -14,7 +14,15 @@ import android.widget.LinearLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
+import org.bytedeco.opencv.opencv_core.MatVector;
+import org.bytedeco.opencv.opencv_stitching.Stitcher;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import java.util.ArrayList;
+
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
 
 public class LocalRecognize extends DialogFragment {
     String appPath = MainActivity.appPath;// TODO
@@ -81,6 +89,13 @@ public class LocalRecognize extends DialogFragment {
             }
         });
 
+        btnWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                combineImg();
+            }
+        });
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,53 +109,49 @@ public class LocalRecognize extends DialogFragment {
         imgLayout.removeAllViews();
 
         LinearLayout.LayoutParams imgParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, img_height);
-        ImageView imageView = new ImageView(getContext());// 重复使用
-        imageView.setLayoutParams(imgParam);
+        imgParam.setMargins(img_margin, img_margin, img_margin, img_margin);
         Bitmap bitmap;// 重复使用
 
         for (int i = 0; i < imgList.size(); i ++) {
+            ImageView imageView = new ImageView(getContext());
+            imageView.setLayoutParams(imgParam);
             bitmap = BitmapFactory.decodeFile(imgList.get(i));
             imageView.setImageBitmap(bitmap);
             imgLayout.addView(imageView);
         }
     }
 
-    public void selectImg() {// 添加图片 TODO
-        // 读取图片
-//        MatVector imgs = new MatVector();
-//        org.bytedeco.opencv.opencv_core.Mat mat1 = imread(img1Path);
-//        org.bytedeco.opencv.opencv_core.Mat mat2 = imread(img2Path);
-//        org.bytedeco.opencv.opencv_core.Mat mat3 = imread(img3Path);
-//        org.bytedeco.opencv.opencv_core.Mat mat4 = imread(img4Path);
-//        imgs.push_back(mat1);
-//        imgs.push_back(mat2);
-//        imgs.push_back(mat3);
-//        imgs.push_back(mat4);
-//
-//        // 合并
-//        Stitcher stitcher = Stitcher.create();
-//        org.bytedeco.opencv.opencv_core.Mat pano = new org.bytedeco.opencv.opencv_core.Mat();
-//        infoLog(mat1.arrayWidth() + " " + mat1.arrayHeight());
-//        infoLog(mat2.arrayWidth() + " " + mat2.arrayHeight());
-//        infoLog(mat3.arrayWidth() + " " + mat3.arrayHeight());
-//        infoLog(mat4.arrayWidth() + " " + mat4.arrayHeight());
-//        int result = stitcher.stitch(imgs, pano);// 合并
-//        infoLog(pano.arrayWidth() + " " + pano.arrayHeight());
-//
-//        // 显示合并的图片 TODO
-//        if (result == 0) {// 如果成功
-//            Mat matBGR = new Mat(pano.address());// TODO 颜色异常
-//            Mat matRGB = new Mat();
-//            Imgproc.cvtColor(matBGR, matRGB, Imgproc.COLOR_BGR2RGB);// 将opencv默认的BGR转成RGB
-//            ImageView imageView1 = findViewById(R.id.img_5);// 合并之后的图片显示的位置
-//            Bitmap bitmap = Bitmap.createBitmap(pano.arrayWidth(), pano.arrayHeight(), Bitmap.Config.RGB_565);
-//            Utils.matToBitmap(matRGB, bitmap);
-//            imageView1.setImageBitmap(bitmap);
-//        }
-    }
+    public void combineImg() {// 添加图片 TODO
+        LinearLayout.LayoutParams imgParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, img_height);
+        imgParam.setMargins(img_margin, img_margin, img_margin, img_margin);
+        Bitmap bitmap;
 
-    public void combineImg() {// 合并图片
-        ;// TODO
+        // 读取图片
+        MatVector imgVector = new MatVector();
+        org.bytedeco.opencv.opencv_core.Mat mat;//  TODO 重复使用
+        for (int i = 0; i < imgList.size(); i ++) {
+            mat = imread(imgList.get(i));
+            imgVector.push_back(mat);
+        }
+
+        // 合并
+        Stitcher stitcher = Stitcher.create();
+        org.bytedeco.opencv.opencv_core.Mat combined = new org.bytedeco.opencv.opencv_core.Mat();
+        int result = stitcher.stitch(imgVector, combined);// 合并
+        MainActivity.infoLog(combined.arrayWidth() + " " + combined.arrayHeight());
+
+        // 显示合并的图片 TODO
+        if (result == 0) {// 如果成功
+            Mat matBGR = new Mat(combined.address());// TODO 颜色异常
+            Mat matRGB = new Mat();
+            Imgproc.cvtColor(matBGR, matRGB, Imgproc.COLOR_BGR2RGB);// 将opencv默认的BGR转成RGB
+            ImageView imageView = new ImageView(getContext());// TODO 合并之后的图片显示的位置
+            imageView.setLayoutParams(imgParam);
+            bitmap = Bitmap.createBitmap(combined.arrayWidth(), combined.arrayHeight(), Bitmap.Config.RGB_565);
+            Utils.matToBitmap(matRGB, bitmap);
+            imageView.setImageBitmap(bitmap);
+            imgLayout.addView(imageView);
+        }
     }
 
 }
