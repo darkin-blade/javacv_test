2e04e3f2f0c5105c2dc535d636b188cd99e53997
lynx <2104934038@qq.com>
2019-11-09 09:05:29 +0800

    todo:缩略图加载

---
+++ app/src/main/java/com/example/javacv/interfaces/ManagerImg.java
@@ -0,0 +1,13 @@
+package com.example.javacv.interfaces;
+
+import android.graphics.Bitmap;
+
+public class ManagerImg {
+    public boolean isImg(String imgPath) {// 判断是否是图片
+        return false;
+    }
+
+    public Bitmap ThumbImg(String imgPath, int width, int height) {// 转换成缩略图
+        return null;
+    }
+}
